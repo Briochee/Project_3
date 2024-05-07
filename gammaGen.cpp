3796fa9044d7ad994ee7d82d75609dea7c54f408
Rio Simpson <djrio33725@gmail.com>
2024-05-06 20:49:46 -0400

    code provided by instructor to make test input files via gamma distribution

---
+++ gammaGen.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <fstream>
+#include <string>
+
+int main()
+{
+  // PARAMETERS TO MODIFY - BE CAREFUL NOT TO OVERWRITE YOUR FILES.
+
+  int observations = 32985; // number of random ints to be generated
+  std::string filename = "name_that_no_important_file_has.txt"; // name of file to create
+  std::string header = "A bunch of integers"; // header for output file
+  int min = 5000; // minimum value
+  double alpha = 4; // shape parameter. Must be >=1. Lower value clusters numbers closer to the min.
+  double beta = 300; // scale parameter. Must be >0. Higher values increase range of generated values.
+
+  // YOU SHOULD NOT NEED TO MODIFY BELOW HERE
+
+  std::ofstream fout("test_input001.txt");
+  // seed RNG
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine generator (seed);
+  // define gamma distribution
+  std::gamma_distribution<double> distribution (alpha,beta);
+
+  // randomly sample gamma distribution and write to file
+  fout << header << std::endl;
+  for (int i=0; i<observations; ++i)
+    fout << int(distribution(generator)+min) << " ";
+  fout << std::endl;
+
+  return 0;
+}
